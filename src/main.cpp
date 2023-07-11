edd6e2dc2163c5c0610564e2ca0dc9948706dfb1
Luis Dietl <interruptedThread@gmail.com>
2023-07-12 00:20:57 +0200

    Added validation layer support

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <cstdint>
+#include <cstring>
+#include <vector>
 #include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -9,6 +11,15 @@
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+const std::vector<const char *> validation_layers = {
+    "VK_LAYER_KHRONOS_validation",
+};
+
+#ifdef NDEBUG
+const bool enable_validation_layers = false;
+#else
+const bool enable_validation_layers = true;
+#endif
 
 class HelloTriangleApplication {
 public:
@@ -30,6 +41,9 @@ private:
   void init_vulkan() { create_instance(); }
 
   void create_instance() {
+    if (!check_validation_layer_support()) {
+      throw std::runtime_error("validation layers requested, but not available!");
+    }
     VkApplicationInfo app_info{};
     app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     app_info.pApplicationName = "Hello Triangle";
@@ -46,9 +60,36 @@ private:
     create_info.enabledExtensionCount = glfw_extension_count;
     create_info.ppEnabledExtensionNames = glfw_extensions;
     create_info.enabledLayerCount = 0;
+    if (enable_validation_layers) {
+      std::cout << "validation layers enabled" << std::endl;
+      create_info.enabledLayerCount = static_cast<uint32_t>(validation_layers.size());
+      create_info.ppEnabledLayerNames = validation_layers.data();
+    } else {
+      create_info.enabledLayerCount = 0;
+    }
     if (vkCreateInstance(&create_info, nullptr, &instance)) {
       throw std::runtime_error("failed to create instance");
     }
+  }
+
+  bool check_validation_layer_support() {
+    uint32_t layer_count;
+    vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+    std::vector<VkLayerProperties> available_layers(layer_count);
+    vkEnumerateInstanceLayerProperties(&layer_count, available_layers.data());
+    for (const char* layer_name : validation_layers) {
+      bool layer_found = false;
+      for (const auto& layer_properties: available_layers) {
+        if (strcmp(layer_name, layer_properties.layerName)) {
+          layer_found = true;
+          break;
+        }
+      }
+      if (!layer_found) {
+        return false;
+      }
+    }
+    return true;
   }
 
   void main_loop() {
