4b35c21fb73a8ae01c6d9daad9183b8efccfee63
Luis Dietl <interruptedThread@gmail.com>
2023-07-14 20:06:30 +0200

    Added command buffer recording

--- src/main.cpp
+++ src/main.cpp
@@ -72,6 +72,8 @@ private:
     create_render_pass();
     create_graphics_pipeline();
     create_framebuffers();
+    create_command_pool();
+    create_command_buffer();
   }
 
   void create_instance() {
@@ -430,6 +432,53 @@ private:
     }
   }
 
+  void create_command_pool() {
+    VkCommandPoolCreateInfo pool_info{};
+    pool_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    pool_info.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    pool_info.queueFamilyIndex = indices.graphics_family.value();
+    if (vkCreateCommandPool(device, &pool_info, nullptr, &command_pool) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create command pool");
+    }
+  }
+
+  void create_command_buffer() {
+    VkCommandBufferAllocateInfo alloc_info{};
+    alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    alloc_info.commandPool = command_pool;
+    alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    alloc_info.commandBufferCount = 1;
+    if(vkAllocateCommandBuffers(device, &alloc_info, &command_buffer) != VK_SUCCESS) {
+      throw std::runtime_error("failed to allocate command buffer");
+    }
+  }
+
+  void record_command_buffer(VkCommandBuffer buffer, uint32_t image_index) {
+    VkCommandBufferBeginInfo begin_info{};
+    begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    begin_info.flags = 0;
+    begin_info.pInheritanceInfo = nullptr;
+    if (vkBeginCommandBuffer(buffer, &begin_info) != VK_SUCCESS) {
+      throw std::runtime_error("failed to begin recording command buffer");
+    }
+    VkRenderPassBeginInfo render_pass_info{};
+    render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    render_pass_info.renderPass = render_pass;
+    render_pass_info.framebuffer = swap_chain_framebuffers[image_index];
+    render_pass_info.renderArea.offset = {0, 0};
+    render_pass_info.renderArea.extent = swap_chain_extent;
+    VkClearValue clear_color = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
+    render_pass_info.clearValueCount = 1;
+    render_pass_info.pClearValues = &clear_color;
+    vkCmdBeginRenderPass(command_buffer, &render_pass_info, VK_SUBPASS_CONTENTS_INLINE);
+    vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphics_pipeline);
+    vkCmdDraw(command_buffer, 3, 1, 0, 0);
+    vkCmdEndRenderPass(command_buffer);
+    if(vkEndCommandBuffer(command_buffer) != VK_SUCCESS) {
+      throw std::runtime_error("failed to record command buffer");
+    }
+  }
+
   bool is_device_suitable(VkPhysicalDevice device) {
     // VkPhysicalDeviceProperties device_properties;
     // VkPhysicalDeviceFeatures device_features;
@@ -581,6 +630,7 @@ private:
   }
 
   void cleanup() {
+    vkDestroyCommandPool(device, command_pool, nullptr);
     for (auto framebuffer: swap_chain_framebuffers) {
       vkDestroyFramebuffer(device, framebuffer, nullptr);
     }
@@ -615,6 +665,8 @@ private:
   VkPipelineLayout pipeline_layout;
   VkPipeline graphics_pipeline;
   std::vector<VkFramebuffer> swap_chain_framebuffers;
+  VkCommandPool command_pool;
+  VkCommandBuffer command_buffer;
 };
 
 int main() {
